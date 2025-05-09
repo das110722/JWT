e1aee69ef5cac1551c36b29d71ba1b842857bdff
das110722 <phuocvu.hcmc@gmail.com>
2025-05-09 19:26:52 +0700

    setup mongoDB-server+mongoDB-use(.ENV) bao mat

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
+dotenv.config();
 const app = express();
+
+const connectToMongo = async () => {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to MongoDB");
+};
+
+connectToMongo();
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
 app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+  console.log("Server is running on port 8000");
 });
